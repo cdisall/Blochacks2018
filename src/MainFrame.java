81f9098f289e4ce0014bb658cdf0054a8be6f71e
Guillaume Vialle <guillaumevialle@guillaumes-air.wireless.concordia.ca>
2018-11-17 21:11:29 -0500

    Test system integration

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,8 +1,16 @@
 import java.awt.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class MainFrame {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		
+		ArrayList<Transaction> transactions = new ArrayList<>();
+		String file = "Summary.pdf";
+		PDFParser parse = new PDFParser(file);
+		transactions = parse.getTransactions();
+		System.out.println(transactions.toString());
+		
 	}
 }
