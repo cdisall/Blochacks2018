a6323e8fcf523ff6eb01ea4661b111559565e257
Daniel Van Acker <danvan4477@gmail.com>
2018-11-17 22:59:48 -0500

    Added addCategory method

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -10,15 +10,23 @@ public class MainFrame {
 		String file = "Summary.pdf";
 		PDFParser parse = new PDFParser(file);
 		transactions = parse.getTransactions();
-//		System.out.println(transactions.toString());
+		//System.out.println(transactions.toString());
+		addCategory(transactions);
 		
-		String merchant = transactions.get(0).getMerchant();
-		String city = transactions.get(0).getCity();
 		
-		System.out.println(merchant);
-		System.out.println(city + "Hello ");
-		String category = REST.getCategory(merchant, city);
-		System.out.println(category);
-		
+	}
+	
+	public static void addCategory(ArrayList<Transaction> transactions) {
+		for (Transaction x : transactions) {
+			String merchant = x.getMerchant();
+			String city = x.getCity();
+			
+			//This step is only required on Windows. Comment out if running on Mac.
+			if (city.charAt(city.length()-2) < 1 || city.charAt(city.length()-2) > 26 ) city = city.substring(0, city.length() - 1);
+			
+			String category = REST.getCategory(merchant, city);
+			
+			x.setCategory(category);
+		}
 	}
 }
